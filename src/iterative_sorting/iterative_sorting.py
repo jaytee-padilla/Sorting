3d812606026e69333fddceaaeba2a7597032e553
Jaytee <Padilla.Jaytee@gmail.com>
2020-04-13 19:40:14 -0700

    Currently working on bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -11,6 +11,8 @@ def selection_sort( arr ):
                 smallest_index = x
 
         # TO-DO: swap
+        # temp variable will hold the value of what's in the current index
+        # the value of cur_index is overwritten by the smallest_index's value, so the temp is needed to record the value of cur_index before it's overwritten
         temp = arr[cur_index]
         arr[cur_index] = arr[smallest_index]
         arr[smallest_index] = temp
@@ -20,7 +22,13 @@ def selection_sort( arr ):
 
 
 # TO-DO:  implement the Bubble Sort function below
+# Loop through array
+# Compare each element to its neighbor
+# If elements in wrong position (relative to each other, swap them)
+# If no swaps performed, stop. Else, go back to the element at index 0 and repeat step 1.
 def bubble_sort( arr ):
+    for i in range(0, len(arr - 1)):
+        print(arr[i])
 
     return arr
 
@@ -32,3 +40,4 @@ def count_sort( arr, maximum=-1 ):
 
 
 print(selection_sort([5, 10, 3, 30, 75, 90, 11, 69]))
+print(bubble_sort([5, 10, 3, 30, 75, 90, 11, 69]))
